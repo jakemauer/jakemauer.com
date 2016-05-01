02f1c4357c4b5ed4f9fd201dc2a116a4f8683fab
Jake Mauer <jake@jakemauer.com>
2016-04-30 18:55:26 -0700

    Include jquery and inlinesvg

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,3 +1,11 @@
 'use strict';
+import jQuery from 'jquery';
+import inlineSVG from 'inline-svg';
 
-document.body.innerHTML = '...';
+jQuery(function(){
+  inlineSVG.init({
+    svgSelector: 'img.svg', // the class attached to all images that should be inlined
+    initClass: 'js-inlinesvg', // class added to <html>
+    storeResults: false // should the results be stored in localStorage
+  })  
+})
