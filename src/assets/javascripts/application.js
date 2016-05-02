e6a53089a8b2251ba219beb69520a997535b1f64
Jake Mauer <jake@jakemauer.com>
2016-05-01 21:01:42 -0700

    No longer using inline svg

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,12 +1,7 @@
 'use strict';
 import $ from 'jquery';
-import inlineSVG from 'inline-svg';
 import animateSVG from './modules/animate-svg'
 
 $(function(){
-  inlineSVG.init({
-    svgSelector: 'img.svg', // the class attached to all images that should be inlined
-    initClass: 'js-inlinesvg', // class added to <html>
-    storeResults: false // should the results be stored in localStorage
-  }, animateSVG)  
+  animateSVG() 
 })
